da0355bad87d10a3137223eb90a3f36994e7059a
yp155136 <yp155136@gmail.com>
2019-11-22 11:42:13 +0800

    gogo

--- graph/KM.cpp
+++ graph/KM.cpp
@@ -1,35 +1,51 @@
-int n , w[MAX][MAX] , lx[MAX] , ly[MAX] , slk[MAX];
-int s[MAX] , t[MAX] , good[MAX];
-int match(int now){
+const int INF = 0x3f3f3f3f;
+const int maxn = 610;
+
+int n, w[maxn][maxn], lx[maxn], ly[maxn], slk[maxn];
+int s[maxn], t[maxn], good[maxn];
+
+int match(int now) {
     s[now] = 1;
-    REP(to , 1 , n + 1){
+    for (int to = 1; to <= n; to ++) {
         if(t[to]) continue;
-        if(lx[now] + ly[to] == w[now][to]){
+        if(lx[now] + ly[to] == w[now][to]) {
             t[to] = 1;
             if(good[to] == 0 || match(good[to]))
-                return good[to] = now , 1;
+                return good[to] = now, 1;
         }
-        else slk[to] = min(slk[to] , lx[now] + ly[to] - w[now][to]);
+        else slk[to] = min(slk[to], lx[now] + ly[to] - w[now][to]);
     }
     return 0;
 }
-int update(){
+void update() {
     int val = INF;
-    REP(i , 1 , n + 1) if(t[i] == 0) val = min(val , slk[i]);
-    REP(i , 1 , n + 1){
+    for (int i = 1; i <= n; i ++) 
+        if(t[i] == 0) val = min(val, slk[i]);
+    for (int i = 1; i <= n; i ++) {
         if(s[i]) lx[i] -= val;
         if(t[i]) ly[i] += val;
     }
 }
-void solve(){
-    REP(i , 1 , n + 1) REP(j , 1 , n + 1)
-        lx[i] = max(lx[i] , w[i][j]);
-    REP(i , 1 , n + 1){
-        MEM(slk , INF);
-        while(1){
-            MEM(s , 0) , MEM(t , 0);
+void run_km() {
+    for (int i = 1; i <= n; i ++) {
+        lx[i] = w[i][1];
+        for (int j = 1; j <= n; j ++)
+            lx[i] = max(lx[i], w[i][j]);
+    }
+    for (int i = 1; i <= n; i ++)
+        ly[i] = 0, good[i] = 0;
+    for (int i = 1; i <= n; i ++) {
+        for (int j = 1; j <= n; j ++) slk[j] = INF;
+        while(1) {
+            for (int j = 1; j <= n; j ++)
+                s[j] = t[j] = 0;
             if(match(i)) break;
             else update();
         }
     }
 }
+/* how_to_use:
+1. put edge in w[i][j]
+2. run_km
+3. match: (good[i], i)
+*/
